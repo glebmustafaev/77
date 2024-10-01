8f6b819d062d3a3889c17cb5a0fe7065bb78eeb7
glebmustafaev <glebmustafaev87@gmail.com>
2024-10-01 17:59:18 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,20 +1,25 @@
 import random
+from itertools import count
+
 r = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 a = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 b = random.choice(r)
-print(b)
 list_ = []
 for i in (a):
     for j in (a):
         if b == i * j:
-            e = [i,j]
+            e = [i, j]
+            e.sort()
             list_.append(e)
 
 for k in (n):
     for m in (n):
         if b == k + m:
-            z=[k,m]
+            z = [k, m]
+            z.sort()
             list_.append(z)
-            break
-print(list_)
+new_list = (' '.join(map(str, list_)))
+
+print(b, '-', new_list)
+
