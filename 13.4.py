ca75f9341ea85e572244c6b13a711f1623f9e0b2
glebmustafaev <glebmustafaev87@gmail.com>
2025-01-28 15:32:09 +0300

    Update 13.4.py

--- 13.4.py
+++ 13.4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-api = '8044660193:AAEhMyoDO5VF1e9T_xqSgQawozYnxHMTAxY'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
